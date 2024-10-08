aae1ec572915894f34c001841e2e9c7a161a2803
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-10-08 16:14:00 -0500

    test reports

--- jest.config.ts
+++ jest.config.ts
@@ -5,5 +5,15 @@ module.exports = {
     '^.+\\.ts$': ['ts-jest', {
       tsconfig: 'tsconfig.json', // Puedes especificar un archivo tsconfig si lo necesitas
     }]
-  }
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-reports',
+        outputName: 'junit.xml'
+      }
+    ]
+  ]
 }
